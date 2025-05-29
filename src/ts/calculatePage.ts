83ec00f6daef58c2ea090305cbfbb3fd85aa8608
KhanUz <72889438+KhanUz@users.noreply.github.com>
2025-05-29 14:22:46 +0800

    start

---
+++ src/ts/calculatePage.ts
@@ -0,0 +1,8 @@
+
+
+function calculatePages(start: number, end: number, onSignlePage: number, bothSides: boolean): number {
+
+    return 1
+}
+
+export { calculatePages }
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/ts/**.ts',
+
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: './dist',
+        hot: true,
+        port: 3000
+    },
+    mode: 'development',
+    devtool: "source-map",
+    resolve: {
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { test: /\.js$/, loader: "source-map-loader" },
+        ],
+    },
+};
